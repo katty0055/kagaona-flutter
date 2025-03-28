4c441a388db982fb736a5dab23cdf498b9f51155
Katteryne Gaona <kagaona@pol.una.py>
2025-03-28 10:46:57 -0300

    primer commit
    Estamos subiendo nuestro primer proyecto a Flutter

---
+++ android/app/src/main/kotlin/com/example/kgaona/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kgaona
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/login_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inicio de Sesión'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Usuario',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Contraseña',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Aquí puedes manejar el inicio de sesión
+              },
+              child: const Text('Iniciar Sesión'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
