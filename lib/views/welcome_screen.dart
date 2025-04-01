5ad5acaf5ff05fe683e273a3e5eaa9978328b1de
arincassia <alejandrabaez2304@gmail.com>
2025-04-01 09:12:30 -0300

    Agrega el boton de cotizaciones

--- lib/views/welcome_screen.dart
+++ lib/views/welcome_screen.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/material.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  List<String> quotes = [];
+  bool isLoading = false;
+
+  Future<void> fetchQuotes() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    // Simulación de una llamada a una API o base de datos
+    await Future.delayed(const Duration(seconds: 2)); // Simula un retraso
+    setState(() {
+      quotes = [
+        'Cotización 1: 100',
+        'Cotización 2: 200',
+        'Cotización 3: 300',
+        'Cotización 4: 400',
+      ];
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,16 +36,26 @@ class WelcomeScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
+          children: [
+            const Text(
               '¡Bienvenido!',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 16),
-            Text(
+            const SizedBox(height: 16),
+            const Text(
               'Has iniciado sesión exitosamente.',
               style: TextStyle(fontSize: 18),
             ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: fetchQuotes,
+              child: const Text('Listar Cotizaciones'),
+            ),
+            const SizedBox(height: 16),
+            if (isLoading)
+              const CircularProgressIndicator()
+            else
+              ...quotes.map((quote) => Text(quote)).toList(),
           ],
         ),
       ),
