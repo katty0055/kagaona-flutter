1fd5a6e82dd0f29f1f14b47e91feffa1f6dd5e81
katty0055 <katteryne0055@gmail.com>
2025-04-10 17:41:44 -0300

    Limpia la pantalla de bienvenida
    -Se eliminan los botones de cotizaciones y los
    textos.

--- lib/views/welcome_screen.dart
+++ lib/views/welcome_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:kgaona/components/side_menu.dart';
-//import 'package:kgaona/views/tareasscreen.dart';
 import 'package:kgaona/views/login_screen.dart';
 import 'package:kgaona/views/tareas_screen.dart';
 
@@ -12,51 +11,6 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-  List<String> quotes = [];
-  bool isLoading = false;
-
-  Future<void> fetchQuotes() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    // Simulación de una llamada a una API o base de datos
-    await Future.delayed(const Duration(seconds: 2)); // Simula un retraso
-    setState(() {
-      quotes = [
-        'Cotización 1: 100',
-        'Cotización 2: 200',
-        'Cotización 3: 300',
-        'Cotización 4: 400',
-      ];
-      isLoading = false;
-    });
-  }
-
-  void _mostrarCotizaciones() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Cotizaciones'),
-          content: SingleChildScrollView(
-            child: Column(
-              children:
-                  quotes.map((quote) => ListTile(title: Text(quote))).toList(),
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cerrar'),
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   int _selectedIndex = 0;
 
@@ -94,53 +48,9 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text('Bienvenido')),
       drawer: const SideMenu(),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              '¡Bienvenido!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Has iniciado sesión exitosamente.',
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {
-                // Acción para listar cotizaciones
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Listando cotizaciones...')),
-                );
-              },
-              child: const Text('Listar Cotizaciones PS'),
-            ),
-            ElevatedButton(
-              onPressed: fetchQuotes,
-              child: const Text('Listar Cotizaciones'),
-            ),
-            const SizedBox(height: 16),
-            if (isLoading)
-              const CircularProgressIndicator()
-            else
-              ...quotes.map((quote) => Text(quote)),
-            ElevatedButton(
-              onPressed: _mostrarCotizaciones,
-              child: const Text("Cotizaciones"),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {
-                // Aquí puedes agregar la lógica para listar cotizaciones
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Listando cotizaciones...')),
-                );
-              },
-              child: const Text('Listar Cotizaciones'),
-            ),
-          ],
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
